0fa875529c8c12d3499c15f1d2d787e351befe1a
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2019-04-13 19:08:01 -0400

    Lowered batch size, updated model accuracy graph

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import classifier_library as cl
 
 
-#for i in range(1, 200):
-    #cl.createClassifier(5)
+for i in range(1, 10):
+    cl.createClassifier(5)
 
 cl.model_rename()
 cl.filter_by_accuracy()
